2352f1f38b7a6039f14742ae59738225beed8e8b
Aryan Mediratta <aryan_02@outlook.com>
2019-10-08 13:55:08 +0530

    Create submission.cpp

---
+++ ZCO14001_video_game/submission.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+	int N{}, H{};
+	cin >> N >> H;
+	vector<int> boxes{};
+	for(int i{}; i < N; i++)
+	{
+		int temp{};
+		cin >> temp;
+		boxes.push_back(temp);
+	}
+	int pos{};
+	bool picked{};
+	while (true)
+	{
+		int ins{};
+		cin >> ins;
+		if(ins == 0)
+		{
+			break;
+		}
+		else {
+			switch(ins)
+			{
+				case 1: // Move left
+					if(pos != 0)
+					{
+						pos--;
+					}
+				break;
+				case 2: // Move right
+					if(pos != N - 1)
+					{
+						pos ++;
+					}
+				break;
+				case 3: // Pick up box
+					if(!picked and boxes.at(pos) != 0)
+					{
+						boxes.at(pos)--;
+						picked = true;
+					}
+				break;
+				case 4: // Drop box
+					if(picked and boxes.at(pos) < H)
+					{
+						boxes.at(pos)++;
+						picked = false;
+					}
+				break;
+			}
+		}
+	}
+	for(int& i : boxes)
+	{
+		cout << i << ' ';
+	}
+
+}
